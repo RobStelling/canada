3eb8b6dda30674279cc70b5d5c0f528587a7475a
Roberto Stelling <roberto@stelling.cc>
2017-03-29 16:40:02 -0300

    More comments, added Border normal/stressed

--- js/canada.js
+++ js/canada.js
@@ -6,6 +6,8 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height"),
     tooltip,
+    BorderNormal = 0.5,
+    BorderStrong = 2,
 /*
  * Creates mapping between provice ID (1 to 13) to the number of athletes in each province
  */
@@ -55,7 +57,7 @@ function ready(error, can) {
     .enter().append("path")
       .attr("class", function(d) {return quantize(rateById.get(d.properties.ID_1)); }) // Class will be used to color each region q0-9 to q8-9
       .attr("d", path)
-      .style("stroke-width", 0.5);
+      .style("stroke-width", BorderNormal);
   createTooltip(); // Creates map tooltip object. It starts as a hidden object defined by the myTip class
 
   function createTooltip()
@@ -69,8 +71,9 @@ function ready(error, can) {
     hookTooltip(); // Creates the hooks for the tooltips
   }
 
-
-
+/*
+ * Describe behaviours for mouseover, mousemove, mouseout and click
+ */
   function hookTooltip()
   {
     var svg = d3.select(".states").selectAll("path")
@@ -79,27 +82,44 @@ function ready(error, can) {
       var tip = d3.select("div.myTip");
       var k;
       var nAthletes = rateById.get(d.properties.ID_1);
-
+/*
+ * English name - French name (or only name if VARNAME_1 == "") and #of Athletes
+ */
       k = d.properties.NAME_1+(d.properties.VARNAME_1 != "" ? "<br>" + d.properties.VARNAME_1 : "") +
           (nAthletes > 0 ? "<br>Athletes/Athètes: "+ rateById.get(d.properties.ID_1).toLocaleString() : "");
       tip.html(k);
-
+/*
+ * Hightlight provice borders
+ */
       d3.selectAll("path").filter(function(dd) {
         return dd.properties.ID_1 == d.properties.ID_1;
-      }).style("stroke-width", "2");
-
+      }).style("stroke-width", BorderStrong);
+/*
+ * Makes sure that tooltip is visible
+ */
       return tooltip.style("visibility", "visible");})
   
     .on("mousemove", function(){
-      return tooltip.style("top", (d3.event.pageY-20)+"px") // Hovers the tooltip ~20px higher and 25px left of the mouse.
+/*
+ * Hovers the tooltip ~20px higher and 25px left of the mouse.
+ */
+      return tooltip.style("top", (d3.event.pageY-20)+"px")
         .style("left",(d3.event.pageX+25)+"px");})
   
     .on("mouseout", function(d){
+/*
+ * Provice borders back to normal
+ */
       d3.selectAll("path").filter(function(dd) {
         return dd.properties.ID_1 == d.properties.ID_1;
-      }).style("stroke-width", "0.5");
-
+      }).style("stroke-width", BorderNormal);
+/*
+ * and hides the tooltip
+ */
       return tooltip.style("visibility", "hidden");}) // Hides the tooltip when finished
+/*
+ * Nothing to do on click at this moment
+ */
     .on("click", function(d){
       return;
     });
