2e16303a990f22b58cf2fbf2301f9eb44842e52e
Roberto Stelling <roberto@stelling.cc>
2017-03-29 16:25:03 -0300

    Cleaned code, added comments

--- js/canada.js
+++ js/canada.js
@@ -1,47 +1,64 @@
 
+/*
+ * Creates svg object and uses width & height from <svg> declaration
+ */
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
-    height = +svg.attr("height");
-
-var rateById = d3.map();
-
-var quantize = d3.scaleQuantize()
+    height = +svg.attr("height"),
+    tooltip,
+/*
+ * Creates mapping between provice ID (1 to 13) to the number of athletes in each province
+ */
+    rateById = d3.map(),
+/*
+ * Will use a Quantize scale for the coloring scheme, max# of athletes is 48, min 0
+ * domain is set to [-5, 48] though to create some leeway room at the beginning of the scale
+ * range is set to 9, as colors are defined in CSS as q?-9 where ? ranges from 0 to 8
+ */
+    quantize = d3.scaleQuantize()
     .domain([-5, 48])
-    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
-
-var projection = d3.geoAlbers();
-    //.scale(985)
-    //.center([5,63.5]);
-    //.translate([width / 2, height / 2]);
-
-
+    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; })),
+/*
+ * Uses Albers projection, probably adequate for a Canada map
+ */
+    projection = d3.geoAlbers();
+/*
+ * Reads map and athletes data
+ */
 d3.queue()
     .defer(d3.json, "./json/CAN_adm1_1_5.json")
     .defer(d3.tsv, "./data/athletes.tsv", function(d) { rateById.set(d.id, +d.athletes); })
     .await(ready);
-
+/*
+ * Map plotting callback
+ */
 function ready(error, can) {
   if (error) throw error;
 
-  //projection.fitSize([width, height], topojson.feature(can, can.objects.CAN_adm1).features);
-
+  //projection.fitSize([width, height], topojson.feature(can, can.objects.CAN_adm1).features); //ignore: Will fitsize when map is projected
+/*
+ * Gets Canada map features
+ */
   var canF = topojson.feature(can, can.objects.CAN_adm1);
-
+/*
+ * Projects map
+ */
   var path = d3.geoPath()
     .projection(projection.fitSize([width, height], canF));
-
+/*
+ * Add svg object with map data
+ */
   svg.append("g")
       .attr("class", "states")
     .selectAll("path")
       .data(canF.features)
     .enter().append("path")
-      .attr("class", function(d) {return quantize(rateById.get(d.properties.ID_1)); })
+      .attr("class", function(d) {return quantize(rateById.get(d.properties.ID_1)); }) // Class will be used to color each region q0-9 to q8-9
       .attr("d", path)
       .style("stroke-width", 0.5);
-  criaTooltip();
-  hookTooltip();
+  createTooltip(); // Creates map tooltip object. It starts as a hidden object defined by the myTip class
 
-  function criaTooltip()
+  function createTooltip()
   {
     tooltip = d3.select("body")
       .append("div")
@@ -49,6 +66,7 @@ function ready(error, can) {
     //   .style("vertical-align", "middle")
       .classed("myTip", true)
       .html("Canadá");
+    hookTooltip(); // Creates the hooks for the tooltips
   }
 
 
@@ -60,58 +78,30 @@ function ready(error, can) {
     .on("mouseover", function(d){
       var tip = d3.select("div.myTip");
       var k;
-      var nAtletas = rateById.get(d.properties.ID_1);
+      var nAthletes = rateById.get(d.properties.ID_1);
 
       k = d.properties.NAME_1+(d.properties.VARNAME_1 != "" ? "<br>" + d.properties.VARNAME_1 : "") +
-          (nAtletas > 0 ? "<br>Athletes/Athètes: "+ rateById.get(d.properties.ID_1).toLocaleString() : "");
-
+          (nAthletes > 0 ? "<br>Athletes/Athètes: "+ rateById.get(d.properties.ID_1).toLocaleString() : "");
       tip.html(k);
+
       d3.selectAll("path").filter(function(dd) {
         return dd.properties.ID_1 == d.properties.ID_1;
       }).style("stroke-width", "2");
-/*      
-      d3.selectAll(".microrregiao")
-        .filter(function(dd){
-          switch (cl.status) {
-            case "populacao":
-            case "brasil": 
-            case "microrregiao": return d.properties.CD_GEOCMI==dd.properties.CD_GEOCMI;
-          }
-        })
-        .style("fill", SELECIONADO);
- */
+
       return tooltip.style("visibility", "visible");})
   
     .on("mousemove", function(){
-      return tooltip.style("top", (d3.event.pageY-20)+"px")
+      return tooltip.style("top", (d3.event.pageY-20)+"px") // Hovers the tooltip ~20px higher and 25px left of the mouse.
         .style("left",(d3.event.pageX+25)+"px");})
   
     .on("mouseout", function(d){
-/*
-      d3.selectAll(".microrregiao")
-        .filter(function(dd){ return this.style.fill == SELECIONADO})
-        .style("fill", function(dd){
-          switch(cl.status) {
-            case "populacao":
-            case "brasil":
-            case "microrregiao": return cor(hashDados[dd.properties.CD_GEOCMI]);
-          }
-      });
- */
       d3.selectAll("path").filter(function(dd) {
         return dd.properties.ID_1 == d.properties.ID_1;
       }).style("stroke-width", "0.5");
 
-      return tooltip.style("visibility", "hidden");})
+      return tooltip.style("visibility", "hidden");}) // Hides the tooltip when finished
     .on("click", function(d){
       return;
     });
   }
-
-/*
-  svg.append("path")
-      .datum(topojson.mesh(can, can.objects.CAN_adm1, function(a, b) { return a !== b; }))
-      .attr("class", "states")
-      .attr("d", path);
-  */
 }
